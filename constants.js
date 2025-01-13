044036e26c56cfa778e1d4e2808b80a94c01b49d
Derek <44646679+DerekfromMadison@users.noreply.github.com>
2025-01-13 13:47:38 -0700

    d

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ import { SUPPORTED_COINS } from 'gridlock-pg-sdk';
 
 
 export const API_KEY = '1234567890'; // Replace with your actual API key
-export const BASE_URL = 'https://32cc-2600-100e-a020-5c3-45b5-b90-d8da-3b4b.ngrok-free.app'; // Replace with your actual base URL
+export const BASE_URL = 'https://1474-50-205-14-114.ngrok-free.app'; // Replace with your actual base URL
 
 export const MONGO_URI = 'mongodb://root:example@172.18.0.1:27017/'; // Ensure the connection string includes the database name
 export const DB_NAME = 'gridlock'; // Database name
